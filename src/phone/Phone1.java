ffa2b59836385a23b7724697a1de4e919be373ed
lizatf <ltuflina@gmail.com>
2024-05-26 21:09:32 +0200

    Class Phone

---
+++ src/phone/Phone1.java
@@ -0,0 +1,63 @@
+package phone;
+
+public class Phone1 implements PrintInfo1 {
+    private String number;
+    private String model;
+    private double weight;
+
+    public Phone1(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone1(String number, String model, double weight) {
+        this(number,model);
+        this.weight = weight;
+    }
+    public Phone1(){}
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public void printInformation() {
+        System.out.println("Телефон модели: " +getModel()+ ", с номером телефона: " +getNumber() + " и весом: " +getWeight());
+    }
+    public String receiveCall(String name){
+        System.out.print("Звонит " +name + " ");
+        return getNumber();
+    }
+    public void receiveCall(String name,String number) {
+        System.out.println("Звонит " + name + " с номером телефона: " + number);
+    }
+    public int sendMessage(String... numbers){
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Сообщение будет отправлено на номер телефона: "+numbers[i]);
+        }
+        return 0;
+    }
+
+    }
+
+
